c96bd4b9e39158f9e13592033128c96869ce4075
Dmitriy Khruschev <khruschevdmitriy@gmail.com>
2018-07-07 22:30:02 +0300

    added components: Main, Header, Menu, Link

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg'
+
+const HeaderWrapper = styled.header`
+	width: 100%;
+	height: 49px;
+	background-color: #242424;
+	opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+	height: 89px;
+	padding-top: 20px;
+`
+const MainWrapper = styled.main`
+	padding-top: 130px;
+	height: 600px;
+	background: url(${url}) no-repeat;
+	backgroun-size: cover;
+`
 class App extends Component {
   render() {
     return (
-      <div className = "wrapper">
-        <SayFullName name="1" surname="2" link="vk.com" />
-        <SayFullName name="3" surname="4" link="facebook.com" />
-        <SayFullName name="5" surname="6" link="#" />
+      <div className="App">
+      	<HeaderWrapper>
+	      	<Grid>
+	        	<Header />
+	        </Grid>
+		</HeaderWrapper>
+      	<MenuWrapper>
+	      	<Grid>
+	        	<Menu />
+	        </Grid>
+		</MenuWrapper>   
+      	<MainWrapper>	
+	      	<Grid>
+	        	<Main />
+	        </Grid>
+		</MainWrapper>  
       </div>
     );
   }
-}
-
-
-function SayFullName(props) {
-  return (
-    <div>
-      <h1> Моё имя {props.name}, фамилия {props.surname} </h1>
-      <a href={props.link}> Ссылка на профиль </a>
-    </div>
-  )
 }
 
 
